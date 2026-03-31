b08292c6f0dab695bfd71832fb30df94f0a3424a
Kushal Batra <24f2000859@ds.study.iitm.ac.in>
2026-04-01 01:10:42 +0530

    Create model.py

---
+++ models/model.py
@@ -0,0 +1,13 @@
+from sklearn.ensemble import RandomForestClassifier
+
+
+def train_model(X, y):
+
+    model = RandomForestClassifier(
+        n_estimators=200,
+        random_state=42
+    )
+
+    model.fit(X, y)
+
+    return model
